91c28338296f0666abd39660f7a6341fe8aa2b45
zoobi <zubairfazalkhan@gmail.com>
2022-02-22 14:11:11 +0500

    First commit
    Some changing in home function

--- Twitterapi/testing/views.py
+++ Twitterapi/testing/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the twitterapi home page.")
+    return HttpResponse("Hello, world. You're at twitterapi home page.")
